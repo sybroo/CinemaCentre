2d7b7772f5d5f27c77b1097e8f8a1b51125c6082
noa van bunge <111687@st-maartenscollege.nl>
2022-01-12 14:24:45 +0100

    ShowAll pagina aangemaakt

--- CinemaCentre/Controllers/HomeController.cs
+++ CinemaCentre/Controllers/HomeController.cs
@@ -27,7 +27,10 @@ namespace CinemaCentre.Controllers
         {
             return View();
         }
-
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
