d7d50b51ddd0a9e6291607ee124dc23428dd0574
syb scholing <110843@st-maartenscollege.nl>
2022-03-16 10:34:51 +0100

    requierd voor contact pagina

--- CinemaCentre/Models/Person.cs
+++ CinemaCentre/Models/Person.cs
@@ -1,16 +1,23 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CinemaCentre.Models
 {
     public class Person
     {
+        [Required(ErrorMessage ="Vul je voornaam alstublieft in!")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage ="Vul je achternaam alstublieft in!")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "Vul je emailadres alstublieft in!")]
         public string Email { get; set; }
 
         public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Vul je opmerking alstublieft in!")]
+        public string Message { get; set; }
 
     }
 }
