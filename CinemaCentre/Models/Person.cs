ba3020fc68f11b822a7519d94a9ecc45c7789f66
syb scholing <110843@st-maartenscollege.nl>
2022-03-16 10:43:43 +0100

    display toegoegd om de strings de juiste naam te geven

--- CinemaCentre/Models/Person.cs
+++ CinemaCentre/Models/Person.cs
@@ -5,17 +5,22 @@ namespace CinemaCentre.Models
 {
     public class Person
     {
+        [Display(Name="Voornaam")]
         [Required(ErrorMessage ="Vul je voornaam alstublieft in!")]
         public string FirstName { get; set; }
 
+        [Display(Name = "achternaam")]
         [Required(ErrorMessage ="Vul je achternaam alstublieft in!")]
         public string LastName { get; set; }
 
+        [Display(Name = "emailadres")]
         [Required(ErrorMessage = "Vul je emailadres alstublieft in!")]
         public string Email { get; set; }
 
+        [Display(Name = "telefoonnummer")]
         public string Phone { get; set; }
 
+        [Display(Name = "opmerking")]
         [Required(ErrorMessage = "Vul je opmerking alstublieft in!")]
         public string Message { get; set; }
 
